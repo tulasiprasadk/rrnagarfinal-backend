484d916af188465d59b6e6f114c73145aac2a74d
RR Nagar Dev <dev@rrnagar.com>
2025-12-14 18:09:40 +0530

    Fix login link visibility, add category images support, fix CSS syntax error

--- models/Category.js
+++ models/Category.js
@@ -1,6 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define('Category', {
     name: { type: DataTypes.STRING, allowNull: false },
-    icon: { type: DataTypes.STRING, defaultValue: "📦" }
+    icon: { type: DataTypes.STRING, defaultValue: "📦" },
+    image: { type: DataTypes.STRING, allowNull: true } // image URL for category
   });
 };
